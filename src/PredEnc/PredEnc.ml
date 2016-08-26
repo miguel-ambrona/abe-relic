bd980d2fbce029571b0117ca78b42868f06ca63c
Miguel Ambrona <miguel.ambrona@imdea.org>
2016-08-26 12:59:19 +0200

    Efficiency improvement for disjunction. New conjunction algorithm.

--- src/PredEnc/PredEnc.ml
+++ src/PredEnc/PredEnc.ml
@@ -98,15 +98,19 @@ module Disjunction_Characterization (C1 : PredEnc_Characterization) (C2 : PredEn
   let rE_matrix (y1,y2) = diag_join (C1.rE_matrix y1) (C2.rE_matrix y2)
   let kE_vector (y1,y2) = (C1.kE_vector y1) @ (C2.kE_vector y2)
   let sD_vector (x1,x2) (y1,y2) =
-    let b1 = C1.sD_vector x1 y1 in
-    let b2 = C2.sD_vector x2 y2 in
-    if C1.predicate x1 y1 then b1 @ (L.map b2 ~f:(fun _ -> Zp.zero))
-    else (L.map b1 ~f:(fun _ -> Zp.zero)) @ b2
+    if C1.predicate x1 y1 then
+      let b1 = C1.sD_vector x1 y1 in
+      b1 @ (mk_list Zp.zero C2.s)
+    else
+      let b2 = C2.sD_vector x2 y2 in
+      (mk_list Zp.zero C1.s) @ b2
   let rD_vector (x1,x2) (y1,y2) =
-    let b1 = C1.rD_vector x1 y1 in
-    let b2 = C2.rD_vector x2 y2 in
-    if C1.predicate x1 y1 then b1 @ (L.map b2 ~f:(fun _ -> Zp.zero))
-    else (L.map b1 ~f:(fun _ -> Zp.zero)) @ b2
+    if C1.predicate x1 y1 then
+      let b1 = C1.rD_vector x1 y1 in
+      b1 @ (mk_list Zp.zero C2.r)
+    else
+      let b2 = C2.rD_vector x2 y2 in
+      (mk_list Zp.zero C1.r) @ b2
 
   let get_witness (x1,x2) (y1,y2) =
     (C1.get_witness x1 y1) @ (C2.get_witness x2 y2)
@@ -189,7 +193,7 @@ module Negation_Characterization (C : PredEnc_Characterization) = struct
   let y_of_string = C.y_of_string
 end
 
-module Conjunction_Characterization  (C1 : PredEnc_Characterization) (C2 : PredEnc_Characterization) = struct
+module Slow_Conjunction_Characterization  (C1 : PredEnc_Characterization) (C2 : PredEnc_Characterization) = struct
     module Neg1 = Negation_Characterization (C1)
     module Neg2 = Negation_Characterization (C2)
     module D = Disjunction_Characterization (Neg1) (Neg2)
@@ -217,6 +221,68 @@ module Conjunction_Characterization  (C1 : PredEnc_Characterization) (C2 : PredE
     let string_of_y = C.string_of_y
     let x_of_string = C.x_of_string
     let y_of_string = C.y_of_string
+end
+
+
+module Conjunction_Characterization  (C1 : PredEnc_Characterization) (C2 : PredEnc_Characterization) = struct
+
+  let diag_join m1 m2 =
+    join_blocks [[ m1; create_matrix Zp.zero ~m:(L.length m1) ~n:(L.length (L.hd_exn m2))];
+                 [ create_matrix Zp.zero ~m:(L.length m2) ~n:(L.length (L.hd_exn m1)); m2 ]];
+
+  type x = C1.x * C2.x
+  type y = C1.y * C2.y
+
+  let predicate (x1,x2) (y1,y2) = (C1.predicate x1 y1) && (C2.predicate x2 y2)
+
+  let s = C1.s + C2.s
+  let r = C1.r + C2.r
+  let w = C1.w + C2.w + 1
+
+  let sE_matrix (x1,x2) =
+    let big_block = diag_join (C1.sE_matrix x1) (C2.sE_matrix x2) in 
+    join_blocks [ [big_block; create_matrix Zp.zero ~m:(L.length big_block) ~n:1 ]]                
+  let rE_matrix (y1,y2) =
+    let big_block = diag_join (C1.rE_matrix y1) (C2.rE_matrix y2) in
+    let last_col  = transpose_matrix [(C1.kE_vector y1) @ (L.map (C2.kE_vector y2) ~f:Zp.neg)] in
+    join_blocks [ [big_block; last_col]]                
+  let kE_vector (y1,y2) = (C1.kE_vector y1) @ (C2.kE_vector y2)
+  let sD_vector (x1,x2) (y1,y2) =
+    let b1 = C1.sD_vector x1 y1 in
+    let b2 = C2.sD_vector x2 y2 in
+    let half = Zp.inv (Zp.add Zp.one Zp.one) in
+    L.map (b1 @ b2) ~f:(Zp.mul half)
+  let rD_vector (x1,x2) (y1,y2) =
+    let b1 = C1.rD_vector x1 y1 in
+    let b2 = C2.rD_vector x2 y2 in
+    let half = Zp.inv (Zp.add Zp.one Zp.one) in
+    L.map (b1 @ b2) ~f:(Zp.mul half)
+          
+  let get_witness (x1,x2) (y1,y2) =
+    if not (C1.predicate x1 y1) then
+      let w1 = C1.get_witness x1 y1 in
+      let two = Zp.add Zp.one Zp.one in
+      (L.map w1 ~f:(Zp.mul two))  @ (mk_list Zp.zero C2.w) @ [Zp.neg Zp.one]
+    else
+      let w2 = C2.get_witness x2 y2 in
+      let two = Zp.add Zp.one Zp.one in
+      (mk_list Zp.zero C1.w) @ (L.map w2 ~f:(Zp.mul two)) @ [Zp.one]
+
+  let set_x = function GenericAttPair(gx1, gx2) -> (C1.set_x gx1, C2.set_x gx2) | _ -> failwith "Pair of Generic Attributes expected"
+  let set_y = function GenericAttPair(gy1, gy2) -> (C1.set_y gy1, C2.set_y gy2) | _ -> failwith "Pair of Generic Attributes expected"
+
+  let sep = "!"
+  let string_of_x (x1,x2) = (C1.string_of_x x1) ^ sep ^ (C2.string_of_x x2)
+  let string_of_y (y1,y2) = (C1.string_of_y y1) ^ sep ^ (C2.string_of_y y2)
+  let x_of_string str =
+    match S.split str ~on:(Char.of_string sep) with
+    | s1 :: s2 :: [] -> (C1.x_of_string s1, C2.x_of_string s2)
+    | _ -> failwith "invalid string"
+  let y_of_string str =
+    match S.split str ~on:(Char.of_string sep) with
+    | s1 :: s2 :: [] -> (C1.y_of_string s1, C2.y_of_string s2)
+    | _ -> failwith "invalid string"
+
 end
     
 
